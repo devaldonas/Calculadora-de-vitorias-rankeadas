a1fb6d4ab467a1c64f0e87258d0fc96ea31edb79
Devaldo <devaldo.nas@gmail.com>
2023-11-20 13:21:54 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function subtrair(vitorias, derrotas) {
+    return vitorias - derrotas;
+
+ }
+  
+  let resultado = subtrair(158, 10); 
+  
+  if(resultado <= 10){
+      console.log("O Herói tem de saldo de " + resultado + " vitórias " + "e está no nível de" + " ferro")
+    
+  }
+      else if(resultado >= 11) if(resultado <= 20){
+  console.log("O Herói tem de saldo de " + resultado + " vitórias " + 	"e está no nível de " +"Bronze ")
+  }
+      else if(resultado >= 21) if(resultado <= 50){
+  console.log("O Herói tem de saldo de " + resultado + " vitórias " + 	"e está no nível de " +"Prata ")
+}
+      else if(resultado >= 51) if(resultado <= 80){
+  console.log("O Herói tem de saldo de " + resultado + " vitórias " + 	"e está no nível de " +"Ouro ")
+}
+      else if(resultado >= 81) if(resultado <= 90){
+  console.log("O Herói tem de saldo de " + resultado + " vitórias " + 	"e está no novel de " +"Diamante ")
+}
+      else if(resultado >= 11) if(resultado <= 20){
+  console.log("O Herói tem de saldo de " + resultado + " vitórias " + 	"e está no nível de " +"Bronze ")
+}
+       else if(resultado >= 91) if(resultado <= 100){
+  console.log("O Herói tem de saldo de " + resultado + " vitórias " + "e está no nível de " +"Lendário ")
+}
+      else if(resultado >= 101)
+  {
+  console.log("O Herói tem de saldo de " + resultado + " vitórias " + "e está no nível de " +"Imortal ")
+  
+  }
